809f92eddc2d98a0126cba8b66f0670e8c751960
Riya <26jainriya@gmail.com>
2024-10-09 22:37:33 -0400

    Staff page edits
    Added cards 'template' and rendering to styles page, added css file to staff folder to style cards

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -1,5 +1,28 @@
 import React from "react";
 import Hero from "@/components/Hero";
+import './styles.css';
+
+interface CardProps {
+  name: string;
+  role: string;
+  description: string;
+}
+
+const Card: React.FC<CardProps> = ({ name, role, description }) => (
+    <div className="body">
+      <h2 >{name}</h2> 
+      <p>{role}</p> 
+      <p>{description}</p> 
+    </div>
+);
+
+const StaffPpl = [
+  {
+    name: "Staff",
+    role: "Role",
+    description: "description"
+  }
+]
 
 export default function StaffPage() {
   return (
@@ -11,6 +34,17 @@ export default function StaffPage() {
         buttonText="Register Now"
         backgroundImage="/background.jpeg"
       />
+
+    <div className="staff">
+        {StaffPpl.map((staff, index) => (
+          <Card
+            key={index}
+            name={staff.name}
+            role={staff.role}
+            description={staff.description}
+          />
+        ))}
+      </div>
     </div>
   );
 }
