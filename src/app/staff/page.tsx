6ee99967620b49e2aa1080e227660400380d4e67
Riya <26jainriya@gmail.com>
2024-11-03 19:14:49 -0500

    changed staff cards

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -8,21 +8,45 @@ interface CardProps {
   description: string;
 }
 
-const Card: React.FC<CardProps> = ({ name, role, description }) => (
-    <div className="body">
-      <h2 >{name}</h2> 
-      <p>{role}</p> 
-      <p>{description}</p> 
-    </div>
+const Card: React.FC<CardProps & { top: string; left: string }> = ({ name, role, description, top, left }) => (
+  <div className="body" style={{ top, left }}>
+    <h2>{name}</h2>
+    <p>{role}</p>
+    <p>{description}</p>
+  </div>
 );
 
 const StaffPpl = [
   {
     name: "Staff",
-    role: "Role",
-    description: "description"
+    role: "[Role]",
+    description: "[description wjoifrjgwjigjojgiwjgrojfoiwjigojwoijrgiowrjogjowirgfwijfrowjfoiwjifjwoifjowijr]",
+    top: "450px",   
+    left: "30px"
+  },
+  {
+    name: "Staff2",
+    role: "Role", 
+    description:"description", 
+    top:"450px", 
+    left:"400px"
+  },
+  {
+    name: "Staff3",
+    role: "Role", 
+    description:"description", 
+    top:"450px", 
+    left:"770px"
+  },
+  {
+    name: "Staff3",
+    role: "Role", 
+    description:"description", 
+    top:"450px", 
+    left:"1140px"
   }
 ]
+
 
 export default function StaffPage() {
   return (
@@ -35,16 +59,19 @@ export default function StaffPage() {
         backgroundImage="/background.jpeg"
       />
 
-    <div className="staff">
-        {StaffPpl.map((staff, index) => (
-          <Card
-            key={index}
-            name={staff.name}
-            role={staff.role}
-            description={staff.description}
-          />
-        ))}
-      </div>
+<div className="staff">
+  {StaffPpl.map((staff, index) => (
+    <Card
+      key={index}
+      name={staff.name}
+      role={staff.role}
+      description={staff.description}
+      top={staff.top}
+      left={staff.left}
+    />
+  ))}
+    </div>
+
     </div>
   );
 }
